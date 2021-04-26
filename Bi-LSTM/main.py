870a56cc3c7db3b68277bf437c3a0aa0ddbb1c87
Andrew Oabel <aoabel@ucsd.edu>
2021-04-25 21:22:47 -0700

    Update main.py

--- Bi-LSTM/main.py
+++ Bi-LSTM/main.py
@@ -5,14 +5,94 @@ Input: Text
 Output: Score between 0 and 1 of fake/real
 """
 # ------------------------------------------------------------------
-import csv
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy
+import random
+import nltk
+from tqdm import tqdm
+from itertools import chain
+from nltk import word_tokenize
+import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+nltk.download('punkt')
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class NewsDataset(Dataset):
+    def __init__(self, labels, text, bos_id, eos_id):
+        self.labels = labels
+        self.text = text
+        self.bos_id = bos_id
+        self.eos_id = eos_id
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, ix):
+        label = self.labels[ix]
+        # Add BOS and EOS to text token sequence
+        tokens = [self.bos_id] + self.text[ix] + [self.eos_id]
+        return label, tokens
+
+class NewsCollator():
+    def __init__(self, pad_id):
+        self.pad_id = pad_id
+
+    def __call__(self, batch):
+        # labels[i] = label for observation i in the batch
+        # tokens[i] = list of token IDs for observation i in the batch
+        labels, tokens = zip(*batch)
+        # Assess necessary padding for tokens
+        max_seq_len = max(len(t) for t in tokens)
+        padded_tokens = [
+            t + [self.pad_id] * (max_seq_len - len(t)) for t in tokens
+        ]
+        # Padding mask: 1 for
+        token_padding_masks = [[1] * len(t) + [0] * (max_seq_len - len(t))
+                               for t in tokens]
+        # Labels, input tokens, padding masks
+        return torch.LongTensor(labels), torch.LongTensor(
+            padded_tokens), torch.BoolTensor(token_padding_masks)
+
+# MODEL
+class BiLSTM(nn.Module):
+    def __init__(self, emb_dim, hidden_dim, num_layers, num_classes,
+                 vocab_size):
+        super(BiLSTM, self).__init__()
+        self.emb_dim = emb_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.num_classes = num_classes
+        self.vocab_size = vocab_size
+
+        # Input embedding
+        self.input_embedding = nn.Embedding(self.vocab_size, self.emb_dim)
+        self.lstm = nn.LSTM(
+            input_size=self.emb_dim,
+            hidden_size=self.hidden_dim,
+            num_layers=1,
+            bias=True,
+            batch_first=True,
+            dropout=0,
+            bidirectional=True)
+
+        # Bidirectional
+        self.scoring_head = nn.Linear(2 * self.hidden_dim, self.num_classes)
+
+    def forward(self, input_tokens):
+        # Embed imput tokens (B x T -> B x T x E)
+        emb_inputs = self.input_embedding(input_tokens)
+        # B x T x E -> B x T x (Dir * H = 2H)
+        # H_0 and C_0 are implicitly set to 0 already
+        lstm_output, _ = self.lstm(emb_inputs)
+        # Take the average across your sequence
+        # B x T x 2H -> B x 2H
+        hidden_representation = lstm_output.mean(dim=1)
+        # Compute your score logits: B x 2H -> B x Nc
+        score = self.scoring_head(hidden_representation)
+        return score
 
 # Hyperparameters
 emb_dim = 128
@@ -23,137 +103,120 @@ num_epochs = 2
 num_layers = 1
 num_classes = 2
 
-class NewsDataset(Dataset):
+# Load raw data with 2 columns
+file_loc = 'news-processed.csv'
+raw_data = pd.read_csv(file_loc)
 
-	def __init__(self):
-		
-		with open('news-processed.csv', newline='') as f:
-			reader = csv.reader(f)
-			data = list(reader)
-			del data[0]
+# Determining vocabulary - list of unique words
+text_tokens = []
+all_labels = []
+vocab = set()
 
-		labels, text = zip(*data)
+# Tokenize each sentence
+for label, h in tqdm(
+        zip(raw_data['label'].values, raw_data['text'].values),
+        total=len(raw_data)):
+    # Invalid values
+    if not isinstance(h, str) or not h.strip():
+        continue
+    tokens = word_tokenize(h)
+    vocab |= set(tokens)
+    text_tokens.append(tokens)
+    # Convert labels to boolean - 0 = real news, 1 = fake news
+    all_labels.append(int(label == 2))
 
-		# Determining vocabulary
-		from itertools import chain
-		vocab = sorted(set(chain.from_iterable([h.split() for h in text])))
-		pad_id = len(vocab)
-		bos_id = len(vocab) + 1
-		eos_id = len(vocab) + 2
-		vocab_size = eos_id + 1
+# Convert tokens into IDs
+vocab_map = dict(zip(sorted(vocab), range(len(vocab))))
+pad_id = len(vocab_map)
+bos_id = len(vocab_map) + 1
+eos_id = len(vocab_map) + 2
+vocab_size = eos_id + 1
+print(f'{len(vocab_map):,} tokens: vocabulary w {vocab_size:,} total tokens')
+text_tokens = [[vocab_map[t] for t in tokens] for tokens in text_tokens]
 
-		# B x T
-		tokenized_text = [
-			[bos_id] + [vocab.index(word) for word in h.split()] + [eos_id] for h in text
-		]
+# Split into training/test
+indices = list(range(len(text_tokens)))
+random.Random(42).shuffle(indices)
 
-		max_len = max(len(h) for h in tokenized_text)
-		sequence_length = max_len
+# 80% for training, 20% for evaluation
+train_cutoff = int(len(indices) * 0.8)
+train_indices = indices[:train_cutoff]
+eval_indices = indices[train_cutoff:]
 
-		padded_input = [
-			h + (max_len - len(h)) * [pad_id]
-			for h in tokenized_text
-		]
+# Training data
+train_ds = NewsDataset(
+    labels=[all_labels[i] for i in train_indices],
+    text=[text_tokens[i] for i in train_indices],
+    bos_id=bos_id,
+    eos_id=eos_id)
+train_loader = DataLoader(
+    train_ds,
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=NewsCollator(pad_id))
 
-		lstm_input = torch.LongTensor(padded_input)
-		input_embedding = nn.Embedding(vocab_size, emb_dim)
-		# B x T x E
-		emb_inputs = input_embedding(lstm_input)
+# Evaluation data
+eval_ds = NewsDataset(
+    labels=[all_labels[i] for i in eval_indices],
+    text=[text_tokens[i] for i in eval_indices],
+    bos_id=bos_id,
+    eos_id=eos_id)
+eval_loader = DataLoader(
+    eval_ds,
+    batch_size=batch_size,
+    shuffle=False,
+    collate_fn=NewsCollator(pad_id))
 
-		labels = [int(i) for i in labels]
-		labels_tensor = torch.LongTensor(labels)
-
-		# B x T
-		self.labels_tensor = labels_tensor
-		self.emb_inputs = emb_inputs
-		self.n_samples = labels_tensor.shape[0]
-		self.sequence_length = max_len
-
-	def __getitem__(self, index):
-		return self.labels_tensor[index], self.emb_inputs[index]
-
-	def __len__(self):
-		return self.n_samples
-
-	def seq_length(self):
-		return self.sequence_length
-
-news_dataset = NewsDataset()
-test_dataset, train_dataset = torch.utils.data.random_split(44898,[8980,35918],generator=torch.Generator().manual_seed(42))
-
-train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
-
-test_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
-
-# MODEL
-
-class BiLSTM(nn.Module):
-	def __init__(self, emb_dim, hidden_dim, num_layers, num_classes):
-		super(BiLSTM, self).__init__()
-		self.hidden_dim = hidden_dim
-		self.num_layers = num_layers
-		self.num_classes = num_classes
-		self.lstm = nn.LSTM(input_size=emb_dim, hidden_size=hidden_dim, num_layers=1, bias=True, batch_first=True, dropout=0, bidirectional=True)
-		self.scoring_head = nn.Linear(2 * hidden_dim, 1)
-
-	def forward(self,emb_inputs):
-		h0 = torch.zeros(self.num_layers*2, emb_inputs.size(0), hidden_dim).to(device) 
-		c0 = torch.zeros(self.num_layers*2, emb_inputs.size(0), hidden_dim).to(device) 
-		# B x T x (Dir * H) -> B x T x 2H
-		lstm_output, _ = self.lstm(emb_inputs,(h0,c0))
-		# Take the average across your sequence
-		# B x T x 2H -> B x 2H
-		hidden_representation = lstm_output.mean(dim=1)
-		# Compute your score
-		score = self.scoring_head(hidden_representation)
-		# Score between 0 and 1
-		score_01 = torch.sigmoid(score)
-		return score_01
-
-model = BiLSTM(emb_dim,hidden_dim,num_layers,num_classes).to(device) 
-
-optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
-
-print(enumerate(train_loader))
+# Create model and load weights into optimizer
+model = BiLSTM(emb_dim, hidden_dim, num_layers, num_classes,
+               vocab_size).to(DEVICE)
+print('{} parameters'.format(
+    sum(p.numel() for p in model.parameters() if p.requires_grad)))
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Training the model
 n_total_steps = len(train_loader)
 for epoch in range(num_epochs):
-	for i, (labels,text) in enumerate(train_loader):
-		print(text.shape)
-		print(labels.shape)
-		text = text.reshape(-1, train_dataset.seq_length(), emb_dim).to(device) 
-		labels = torch.unsqueeze(labels,-1)
-		labels = labels.type_as(text).to(device) 
-		
-		score = model(text)
-		loss = F.binary_cross_entropy_with_logits(input=score, target=labels)
+    for i, (labels, input_tokens, padding_mask) in tqdm(
+            enumerate(train_loader), total=n_total_steps):
+        # Move to correct device
+        labels = labels.to(DEVICE)
+        input_tokens = input_tokens.to(DEVICE)
+        padding_mask = padding_mask.to(DEVICE)
 
-		optimizer.zero_grad()
-		loss.backward(retain_graph=True)
-		optimizer.step()
+        # Run model - score is B x Nc
+        score = model(input_tokens)
 
-		if (i+1) % 1000 == 0:
-			print(f'Epoch [{epoch+1}/{num_epochs}]')
-			print(f'Step [{i+1}/{n_total_steps}]')
-			print(f'Loss: {loss.item():.4f}')
+        # Compute loss between B x Nc score and B labels
+        loss = F.cross_entropy(input=score, target=labels)
 
-		
+        # Backwards step
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
+
+        # Logging
+        if (i + 1) % 50 == 0:
+            print(f'Epoch [{epoch+1}/{num_epochs}]')
+            print(f'Step [{i+1}/{n_total_steps}]')
+            print(f'Loss: {loss.item():.4f}')
+
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
 with torch.no_grad():
-	n_correct = 0
-	n_samples = 0
-	for labels, text in test_loader:
-		text = text.reshape(-1, train_dataset.seq_length(), emb_dim).to(device) 
-		labels = torch.unsqueeze(labels,-1)
-		labels = labels.type_as(text).to(device) 
-		
-		scores = model(text)
-		# max returns (value ,index)
-		_, predicted = torch.max(scores.data, 1)
-		n_samples += labels.size(0)
-		n_correct += (predicted == labels).sum().item()
+    n_correct = 0
+    n_samples = 0
+    for i, (labels, input_tokens, padding_mask) in tqdm(
+            enumerate(eval_loader), total=len(eval_ds)):
+        # Move to correct device
+        labels = labels.to(DEVICE)
+        input_tokens = input_tokens.to(DEVICE)
+        padding_mask = padding_mask.to(DEVICE)
 
-	acc = 100.0 * n_correct / n_samples
-	print(f'Accuracy of the network is: {acc} %')
+        # Run model
+        score = model(input_tokens)
+        predicted = torch.argmax(score, dim=-1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network is: {acc} %')
